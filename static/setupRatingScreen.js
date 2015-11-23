1f3895d52cce639e88d4cd43bb71cdff3330606a
Utkarsh Shah <utkarshshah007@gmail.com>
2015-11-23 16:13:48 -0500

    tabbing genres on suggestions page

--- static/setupRatingScreen.js
+++ static/setupRatingScreen.js
@@ -3,17 +3,17 @@ $(function(){
 	$('#movie2').slider();
 
 	$("#submitButton").click(function(){
-	console.log("Button Clicked")
-	var value = $('#movie1').slider('getValue');
-    var value2 = $('#movie2').slider('getValue');
+		console.log("Button Clicked")
+		var value = $('#movie1').slider('getValue');
+	    var value2 = $('#movie2').slider('getValue');
 
-	outdata = {};
-	outdata['movie1'] = value;
-	outdata['movie2'] = value2;
+		outdata = {};
+		outdata['movie1'] = value;
+		outdata['movie2'] = value2;
 
-    $.post("setup-rating", 	outdata, function (data) {
-    	window.location.href = data;
-    });
-});
+	    $.post("setup-rating", 	outdata, function (data) {
+	    	window.location.href = data;
+	    });
+	});
 });
 
