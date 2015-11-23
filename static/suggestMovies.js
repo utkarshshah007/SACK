c21e792cb7aabe7fc56da0d06397d419629bd71d
Utkarsh Shah <utkarshshah007@gmail.com>
2015-11-23 17:12:14 -0500

    choosing genre makes post request

--- static/suggestMovies.js
+++ static/suggestMovies.js
@@ -1,6 +1,14 @@
 $(function(){
 	$(".list-group-item").click(function() {
-			$(".list-group-item").removeClass("active")
-			$(this).addClass("active")
+		$(".list-group-item").removeClass("active")
+		$(this).addClass("active")
+
+		var outdata = {};
+		outdata['genre'] = $(this).text();
+		console.log(outdata);
+
+		$.post("suggestions", outdata, function (data) {
+    		console.log("SUCCESS");
+    	});
 	});
 });
