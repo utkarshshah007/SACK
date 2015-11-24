3014fd6cecff43a07a1950ad48984c288ccea2c5
Utkarsh Shah <utkarshshah007@gmail.com>
2015-11-23 19:33:15 -0500

    JS to accept varying movie ratings

--- static/setupRatingScreen.js
+++ static/setupRatingScreen.js
@@ -1,24 +1,12 @@
 $(function(){
-	$('#movie1').slider();
-	$('#movie2').slider();
-	$('#movie3').slider();
-	$('#movie4').slider();
-	$('#movie5').slider();
+	$('.movie-slider').slider();
 
-	$("#submitButton").click(function(){
-		console.log("Button Clicked")
-		var value = $('#movie1').slider('getValue');
-	    var value2 = $('#movie2').slider('getValue');
-	    var value3 = $('#movie3').slider('getValue');
-	    var value4 = $('#movie4').slider('getValue');
-	    var value5 = $('#movie5').slider('getValue');
-
+	$('#submitButton').click(function(){
 		outdata = {};
-		outdata['movie1'] = value;
-		outdata['movie2'] = value2;
-		outdata['movie3'] = value3;
-		outdata['movie4'] = value4;
-		outdata['movie5'] = value5;
+
+		$('.movie-slider').each( function(index) {
+			outdata[$(this).attr('id')] = $(this).slider('getValue');
+		});	
 
 	    $.post("setup-rating", 	outdata, function (data) {
 	    	window.location.href = data;
