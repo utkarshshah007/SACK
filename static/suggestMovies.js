50165aea6630d782c9268093be0f6e02e359b870
Utkarsh Shah <utkarshshah007@gmail.com>
2015-11-23 20:16:03 -0500

    fixed first click suggestion

--- static/suggestMovies.js
+++ static/suggestMovies.js
@@ -1,11 +1,23 @@
 $(function(){
+
+	$(document).ready(function() {
+		var firstdata = {};
+		firstdata['genre'] = $('.active').text();
+		$.post("suggestions", firstdata, function (data) {
+
+	    		$("#movieYear").text(data["year"]);
+	    		$("#movieName").text(data["title"]);
+
+	    });
+	});
+
+
 	$(".list-group-item").click(function() {
 		$(".list-group-item").removeClass("active")
 		$(this).addClass("active")
 
 		var outdata = {};
 		outdata['genre'] = $(this).text();
-		console.log(outdata);
 
 		$.post("suggestions", outdata, function (data) {
 
